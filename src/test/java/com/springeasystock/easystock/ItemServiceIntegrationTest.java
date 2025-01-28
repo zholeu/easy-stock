062318ade6eeb34e93f9580cd393a2e33eadb068
Zarina Tlupova <96023685+zarinatlupova@users.noreply.github.com>
2025-01-28 16:36:25 +0300

    Update ItemServiceIntegrationTest.java

--- src/test/java/com/springeasystock/easystock/ItemServiceIntegrationTest.java
+++ src/test/java/com/springeasystock/easystock/ItemServiceIntegrationTest.java
@@ -1,14 +1,22 @@
 package com.springeasystock.easystock;
 
 import com.springeasystock.easystock.exception.CustomNotFoundException;
+import com.springeasystock.easystock.mapper.ItemMapper;
+import com.springeasystock.easystock.model.Item;
+import com.springeasystock.easystock.record.EmployeeDTO;
 import com.springeasystock.easystock.record.ItemDTO;
 import com.springeasystock.easystock.record.RoleDTO;
+import com.springeasystock.easystock.repo.EmployeeRepository;
+import com.springeasystock.easystock.repo.ItemRepository;
 import com.springeasystock.easystock.service.impl.ItemServiceImpl;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -23,10 +31,10 @@ public class ItemServiceIntegrationTest {
             .withDatabaseName("easystock")
             .withUsername("postgres")
             .withPassword("user");
-
     @Autowired
     private ItemServiceImpl itemService;
-
+    @Autowired
+    private ItemRepository itemRepository;
     @BeforeAll
     public static void setUp() {
         postgresContainer.start();
@@ -43,13 +51,10 @@ public class ItemServiceIntegrationTest {
 
     @Test
     public void testCreateItem() {
-        // Create a dummy order list to associate with the item
         ItemDTO itemDTO = new ItemDTO(
                 null, "Item A", "Supplier A", 5.0f, 10.5f, 1, 1, 1, new HashSet<>()
         );
-
         ItemDTO createdItem = itemService.createItem(itemDTO);
-
         assertNotNull(createdItem);
         assertEquals(itemDTO.name(), createdItem.name());
         assertEquals(itemDTO.supplier(), createdItem.supplier());
@@ -66,9 +71,7 @@ public class ItemServiceIntegrationTest {
                 null, "Item B", "Supplier B", 7.5f, 15.0f, 2, 2, 2, new HashSet<>()
         );
         ItemDTO createdItem = itemService.createItem(itemDTO);
-
         ItemDTO fetchedItem = itemService.getItemById(createdItem.id());
-
         assertNotNull(fetchedItem);
         assertEquals(createdItem.id(), fetchedItem.id());
     }
@@ -79,10 +82,18 @@ public class ItemServiceIntegrationTest {
                 null, "Item C", "Supplier C", 6.0f, 20.0f, 3, 3, 3, new HashSet<>()
         );
         ItemDTO createdItem = itemService.createItem(itemDTO);
-
         itemService.deleteItem(createdItem.id());
-
         assertThrows(CustomNotFoundException.class, () -> itemService.getItemById(createdItem.id()));
+    }
+    @Test
+    public void testGetAllItems() {
+        ItemDTO itemDTO = new ItemDTO(null, "Item A", "Supplier A", 5.0f, 10.5f, 1, 1, 1, new HashSet<>());
+        ItemDTO itemDTO2 = new ItemDTO(null, "Item A", "Supplier A", 5.0f, 10.5f, 1, 1, 1, new HashSet<>());
+        itemService.createItem(itemDTO);
+        itemService.createItem(itemDTO2);
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<ItemDTO> itemDTOPage = itemService.getAllItems(pageable);
+        assertNotNull(itemDTOPage);
     }
 
 }
