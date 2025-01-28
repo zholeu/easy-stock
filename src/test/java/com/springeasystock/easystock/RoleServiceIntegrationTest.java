113395acf26c4a1566bfb3c5bffd00ebc61650fd
Zarina Tlupova <96023685+zarinatlupova@users.noreply.github.com>
2025-01-28 16:37:05 +0300

    Update RoleServiceIntegrationTest.java

--- src/test/java/com/springeasystock/easystock/RoleServiceIntegrationTest.java
+++ src/test/java/com/springeasystock/easystock/RoleServiceIntegrationTest.java
@@ -1,8 +1,15 @@
 package com.springeasystock.easystock;
 
+import com.springeasystock.easystock.model.Role;
+import com.springeasystock.easystock.record.ItemDTO;
 import com.springeasystock.easystock.record.RoleDTO;
+import com.springeasystock.easystock.repo.OrderListRepository;
+import com.springeasystock.easystock.repo.RoleRepository;
 import com.springeasystock.easystock.service.impl.RoleServiceImpl;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 import com.springeasystock.easystock.exception.CustomNotFoundException;
@@ -10,6 +17,9 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -21,7 +31,8 @@ public class RoleServiceIntegrationTest {
 
     @Autowired
     private RoleServiceImpl roleService;
-
+    @Autowired
+    private RoleRepository roleRepository;
     @BeforeAll
     public static void setUp() throws InterruptedException {
         postgresContainer.start();
@@ -42,11 +53,8 @@ public class RoleServiceIntegrationTest {
 
     @Test
     public void testCreateRole() {
-        // Creating a mock RoleDTO for testing
         RoleDTO roleDTO = new RoleDTO(1L, "Admin", null);
-
         RoleDTO createdRole = roleService.createRole(roleDTO);
-
         assertNotNull(createdRole);
         assertEquals(roleDTO.name(), createdRole.name());
     }
@@ -55,9 +63,7 @@ public class RoleServiceIntegrationTest {
     public void testGetRoleById() {
         RoleDTO roleDTO = new RoleDTO(1L, "User", null);
         RoleDTO createdRole = roleService.createRole(roleDTO);
-
         RoleDTO fetchedRole = roleService.getRoleById(createdRole.id());
-
         assertNotNull(fetchedRole);
         assertEquals(createdRole.id(), fetchedRole.id());
     }
@@ -66,9 +72,7 @@ public class RoleServiceIntegrationTest {
     public void testDeleteRole() {
         RoleDTO roleDTO = new RoleDTO(2L, "Manager", null);
         RoleDTO createdRole = roleService.createRole(roleDTO);
-
         roleService.deleteRole(createdRole.id());
-
         assertThrows(CustomNotFoundException.class, () -> roleService.getRoleById(createdRole.id()));
     }
 
@@ -76,12 +80,27 @@ public class RoleServiceIntegrationTest {
     public void testUpdateRole() {
         RoleDTO roleDTO = new RoleDTO(1L, "Editor", null);
         RoleDTO createdRole = roleService.createRole(roleDTO);
-
         RoleDTO updatedRole = new RoleDTO(1L, "SuperEditor", null);
-
         RoleDTO updatedRoleResult = roleService.updateRole(createdRole.id(), updatedRole);
-
         assertNotNull(updatedRoleResult);
         assertEquals(updatedRole.name(), updatedRoleResult.name());
+    }
+    @Test
+    public void testGetAllRoles() {
+        RoleDTO roleDTO = new RoleDTO(1L, "Admin", null);
+        RoleDTO roleDTO2 = new RoleDTO(1L, "Admin", null);
+        roleService.createRole(roleDTO);
+        roleService.createRole(roleDTO2);
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<RoleDTO> roles = roleService.getAllRoles(pageable);
+        assertNotNull(roles);
+    }
+    @Test
+    public void testExistsById() {
+        Role role = new Role();
+        role.setName("Admin");
+        roleRepository.save(role);
+        assertTrue(roleService.existsById(role.getId()));
+        assertFalse(roleService.existsById(999L));
     }
 }
